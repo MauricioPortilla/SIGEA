0b37e290758346f2cffde7be8d5540b139d4fe06
Mauricio Cruz Portilla <mauricioportilla189@hotmail.com>
2020-06-08 19:34:34 -0500

    Use case 33: Unit test added

---
+++ SIGEA/SIGEA.Pruebas/ConsultarGastosPruebas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIGEABD;
+
+namespace SIGEA.Pruebas {
+    [TestClass]
+    public class ConsultarGastosPruebas {
+        /// <summary>
+        /// Prueba que verifica que se obtienen los gastos de un evento.
+        /// </summary>
+        [TestMethod]
+        public void ConsultarGastosPrueba() {
+            using (SigeaBD sigeaBD = new SigeaBD()) {
+                var gastos = sigeaBD.Gasto.Where(gasto => gasto.id_evento == 1);
+                Assert.IsTrue(gastos.Count() > 0);
+            }
+        }
+    }
+}
